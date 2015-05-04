015451a66b66160e3598e3cc5f8041a02dc1755c
Nick Sagona <nick@nolainteractive.com>
2015-05-04 18:39:00 -0500

    Patch applyFilter function

--- src/AbstractForm.php
+++ src/AbstractForm.php
@@ -318,10 +318,8 @@ abstract class AbstractForm extends Child implements \ArrayAccess
     protected function applyFilter(&$array, $call, $params = [])
     {
         array_walk_recursive($array, function(&$value, $key, $userdata) {
-            if (isset($this->fields[$key])) {
-                $params = array_merge([$value], $userdata[1]);
-                $value  = call_user_func_array($userdata[0], $params);
-            }
+            $params = array_merge([$value], $userdata[1]);
+            $value  = call_user_func_array($userdata[0], $params);
         }, [$call, $params]);
     }
 
