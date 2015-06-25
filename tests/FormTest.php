9bfef61c03bd8d3aafd7bed27ff6410194056250
Nick Sagona <nick@nolainteractive.com>
2015-06-25 18:06:17 -0500

    Work on unit tests

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -3,6 +3,7 @@
 namespace Pop\Form\Test;
 
 use Pop\Form\Form;
+use Pop\Form\Element;
 
 class FormTest extends \PHPUnit_Framework_TestCase
 {
@@ -233,6 +234,165 @@ class FormTest extends \PHPUnit_Framework_TestCase
             'type'  => 'file',
             'label' => 'File:'
         ]]]);
+    }
+
+    public function testSetFieldValues()
+    {
+
+        $fields = [
+            'username' => [
+                'type'       => 'text',
+                'label'      => 'Username:'
+            ],
+            'email' => [
+                'type'       => 'text',
+                'label'      => 'Email:'
+            ],
+            'cars' => [
+                'type'  => 'radio',
+                'label' => 'Cars',
+                'value' => [
+                    'Chevy'   => 'Chevy',
+                    'Ford' => 'Ford'
+                ],
+                'marked' => 'Chevy'
+            ],
+            'fruit' => [
+                'type'  => 'select',
+                'label' => 'Fruit',
+                'value' => [
+                    'Apples'   => 'Apples',
+                    'Oranges' => 'Oranges'
+                ],
+                'marked' => 'Oranges'
+            ],
+            'colors' => [
+                'type'  => 'radio',
+                'label' => 'Colors',
+                'value' => [
+                    'Red'   => 'Red',
+                    'Green' => 'Green',
+                    'Blue'  => 'Blue'
+                ],
+                'marked' => 'Red'
+            ],
+            'comments' => [
+                'type'       => 'textarea',
+                'label'      => 'Comments:'
+            ],
+            'submit' => [
+                'type'       => 'submit',
+                'value'      => 'SUBMIT'
+            ]
+        ];
+
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form($fields);
+        $form->setErrorDisplay('div', ['class' => 'error'], true);
+        $form->setFieldValues([
+            'username' => 'my_username',
+            'email'    => 'test@test.com',
+            'cars'     => 'Ford',
+            'fruit'    => 'Apples',
+            'colors'   => ['Green', 'Blue']
+        ]);
+        $this->assertEquals('my_username', $form->username);
+        $this->assertEquals('test@test.com', $form->email);
+    }
+
+    public function testInsertElementBefore()
+    {
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form([
+            'username' => [
+                'type'     => 'text',
+                'label'    => 'Username:',
+                'required' => true
+            ]
+        ]);
+        $form->insertElementBefore('username', new Element\Input('my_input'));
+        $this->assertInstanceOf('Pop\Form\Form', $form);
+    }
+
+    public function testInsertElementBeforeException()
+    {
+        $this->setExpectedException('Pop\Form\Exception');
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->insertElementBefore('username', new Element\Input('my_input'));
+    }
+
+    public function testInsertElementAfter()
+    {
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form([
+            'username' => [
+                'type'     => 'text',
+                'label'    => 'Username:',
+                'required' => true
+            ]
+        ]);
+        $form->insertElementAfter('username', new Element\Input('my_input'));
+        $this->assertInstanceOf('Pop\Form\Form', $form);
+    }
+
+    public function testInsertElementAfterException()
+    {
+        $this->setExpectedException('Pop\Form\Exception');
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->insertElementAfter('username', new Element\Input('my_input'));
+    }
+
+    public function testAddElements()
+    {
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->addElements([
+            new Element\CheckboxSet('my_checkbox', ['Red' => 'Red', 'White' => 'White', 'Blue' => 'Blue'])
+        ]);
+        $this->assertEquals(1, count($form->elements()));
+    }
+
+    public function testAddElementsException()
+    {
+        $this->setExpectedException('Pop\Form\Exception');
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->addElements(['badelement']);
+    }
+
+    public function testGetElement()
+    {
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->addElements([
+            new Element\CheckboxSet('my_checkbox', ['Red' => 'Red', 'White' => 'White', 'Blue' => 'Blue'])
+        ]);
+        $this->assertInstanceOf('Pop\Form\Element\CheckboxSet', $form->element('my_checkbox'));
+    }
+
+    public function testGetElementIndex()
+    {
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->addElements([
+            new Element\CheckboxSet('my_checkbox', ['Red' => 'Red', 'White' => 'White', 'Blue' => 'Blue'])
+        ]);
+        $this->assertEquals(0, $form->getElementIndex('my_checkbox'));
+    }
+
+    public function testRemoveElement()
+    {
+        $_SERVER['REQUEST_URI'] = '/process';
+        $form = new Form();
+        $form->addElements([
+            new Element\Input('my_input'),
+            new Element\CheckboxSet('my_checkbox', ['Red' => 'Red', 'White' => 'White', 'Blue' => 'Blue'])
+        ]);
+        $form->removeElement('my_checkbox');
+        $form->removeElement('my_input');
+        $this->assertEquals(0, count($form->elements()));
     }
 
 }
