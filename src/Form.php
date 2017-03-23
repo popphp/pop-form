57e660db5674b2b5cf20e8759462fb942e9d298b
Nick Sagona <nick@nolainteractive.com>
2017-03-23 17:00:06 -0500

    Patch setFieldValues()

--- src/Form.php
+++ src/Form.php
@@ -561,7 +561,10 @@ class Form extends Child implements \ArrayAccess, \Countable, \IteratorAggregate
         foreach ($fields as $name => $value) {
             if (isset($values[$name])) {
                 $this->setFieldValue($name, $values[$name]);
-            } else {
+            } else if (!($this->getField($name) instanceof Element\Button) &&
+                !($this->getField($name) instanceof Element\Input\Button) &&
+                !($this->getField($name) instanceof Element\Input\Submit) &&
+                !($this->getField($name) instanceof Element\Input\Reset)) {
                 $this->getField($name)->resetValue();
             }
         }
